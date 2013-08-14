312ab41016a36209dc37c568c1f03c045702a304
acutesoftware <djmurray@acutesoftware.com.au>
2013-08-14 19:13:47 +0930

    first commit

---
+++ cdd_config.py
@@ -0,0 +1,8 @@
+# cdd_config.py
+# configuration settings for code_doc_data
+
+database = 'localhost'
+username = 'username'
+password = 'hunter2'
+
+csv_folder = ''
---
+++ code_doc_data.py
@@ -0,0 +1,32 @@
+# code_doc_data.py  written by Duncan Murray 14/8/2013 (C) Acute Software
+# This is the python implementation of code_doc_data which allows you to
+# automatically add documentation to a database (or CSV file) of your batch
+# processes.
+#   --- SETUP ---
+#   # edit the cdd_config.py to chose how to keep logged information
+#
+#   --- USAGE DURING BATCH PROCESS ---
+#   import code_doc_data as cdd
+#   cdd.InitProject('TEST', 'Test project to see how it works', '')
+#   cdd.CopyFile ('test.doc', 'test_backup.doc', 0)
+#   # remaining batch process (ETL load, file transfers, web downloads)
+#
+#  --- REPORTING ---
+
+import cdd_config
+import os, sys, csv
+
+def InitProject(proj_id, proj_name, srcFolder):
+    # initialise the database / folder for a new project
+    # this wipes existing data for 'proj_id' to allow the 
+    # batch process in your code to reenter the details
+    # with updated metadata
+    # DELETE FROM dataStore WHERE database.proj_id = proj_id
+    # INSERT INTO dataStore.listOfProjects (proj_id, proj_name, srcFolder)
+
+def CopyFile(src, dst, verify=1):
+    # copies a file, and logs the details
+    # shutil.copyfile(src, dst)                             # do the file copy
+    # todo                                                  # verify if needed
+    # dataStore.fileCopies.Add(fname, srcPath, destPath)    # log the file copy
+    
